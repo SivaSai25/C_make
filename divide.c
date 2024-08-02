ec9944c4ca2e1db7f03ea1ac6eb662154452e2dd
siva <sivasai2511@gmail.com>
2024-08-02 23:42:36 +0530

    my first commit

---
+++ divide.c
@@ -0,0 +1,11 @@
+//C program to divide two numbers.
+
+#include<stdio.h>
+int main()
+{
+  float n1,n2,div;
+  printf("Enter two numbers:");// Take input from user
+  scanf("%f%f",&n1,&n2);
+  div=n1/n2; // for divide users number
+  printf("Division of two numbers=%.3f",div);// give output of the users input
+}
